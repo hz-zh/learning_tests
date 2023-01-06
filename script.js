de3731f79f7e012e630ee035e21922d5743d6700
hz-zh <115835509+hz-zh@users.noreply.github.com>
2023-01-05 22:02:23 -0500

    Modify index and script with an add 7 function.
    Also update how checkNum is called. checkNum must return "undefined" for any non-numeric values.
    Add if statement in print function to only update <p> text with add7 function callback when
    checkSum does not return undefined.

--- script.js
+++ script.js
@@ -2,17 +2,28 @@
 const input = document.querySelector('.numberInput');
 const para = document.querySelector('p');
 
-document.getElementById("square").addEventListener("click", squareNum);
-/*input.addEventListener("change", printAnswer);*/
+document.getElementById("square").addEventListener("click", print);
+input.addEventListener("change", print);
+
+function add7(num) { 
+  return num + 7;}
+
+function print(){
+   num = parseFloat(input.float);
+   if ( checkNum(parseFloat(input.value)) != undefined )
+      { para.textContent = `${add7(checkNum(parseFloat(input.value)))}`; }
+   }
 
 function checkNum(num) {
-
    if (isNaN(num)) {
       para.textContent = "You need to enter a number!";
       } 
-      else { return num; }
-   }
+   else if (!isNaN(num)){ 
+         return num;
+      };
+}
 
+/*
 function squared(num) {
    let newnum = checkNum(num);
    return newnum * newnum;
@@ -23,4 +34,4 @@ function squareNum()
    const num = parseFloat(input.value);
    para.textContent = `${num} squared is ${squared(num)}.`;
 }
-
+*/
